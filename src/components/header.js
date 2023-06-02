9d79d0609dbd91c343e6efce948933aa19122139
Chaerins <cherin8887@gmail.com>
2023-06-02 14:59:20 +0900

    헤더 css모듈화

--- src/components/header.js
+++ src/components/header.js
@@ -3,6 +3,7 @@ import teamLogo from '../image/team_logo.png';
 import {Link} from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import '../css/header.css';
 
 
 const Header = () => {
