5af2802cc06b5271ea835e094520e5a6e279107c
Chaerins <cherin8887@gmail.com>
2023-06-12 11:45:06 +0900

    itemmain css 모듈화 완

--- src/routes/itemmain.js
+++ src/routes/itemmain.js
@@ -1,7 +1,6 @@
 import { React, useState } from 'react';
 import { MainpageItem1, MainpageItem2, MainpageItem3 } from '../elements/mainpageitem';
 import Itemmainbutton from '../elements/itemmainbutton';
-import mainimg1 from '../image/temp_main_img_tonedown.jpg';
 import buttondata from '../elements/buttondata';
 import Topimg from '../elements/topimg';
 import style from '../css/itemmain.module.css';
@@ -18,44 +17,44 @@ const Itemmain = () => {
     return (
         <>
             <Topimg/>
-            <div className='middle_big_container'>
-                <div className='middle_big_container_textbox'>
-                    <div className='middle_left_box_child1 middle_slide_up1'>BEST ITEMS</div>
-                    <div className='middle_left_box_child2 middle_slide_up2'>"sale for this season"</div>
+            <div className={style.middle_big_container}>
+                <div className={style.middle_big_container_textbox}>
+                    <div className={`${style.middle_left_box_child1} ${style.middle_slide_up1}`}>BEST ITEMS</div>
+                    <div className={`${style.middle_left_box_child2} ${style.middle_slide_up2}`}>"sale for this season"</div>
                 </div>
-                <div className='slide_box_container'>
-                    <ul className='slide_box'>
+                <div className={style.slide_box_container}>
+                    <ul className={style.slide_box}>
                         {items.map((Item, index) => (
-                            <div className='slide_boximg' key={index}>
+                            <div className={style.slide_boximg} key={index}>
                                 <Item />
                             </div>
                         ))}
                     </ul>
                 </div>
             </div>
-            <div className='slide_horizon'></div>
+            <div className={style.slide_horizon}></div>
 
-            <div className='bottom_big_container'>
-                <div className='menubox_container'>
-                    <div className='menu__btn__container'>
+            <div className={style.bottom_big_container}>
+                <div className={style.menubox_container}>
+                    <div className={style.menu__btn__container}>
                         {buttondata.map((button, index) => (
                             <Itemmainbutton
                                 key={index}
                                 name={button.name}
                                 count={button.count}
                                 data_category={button.data_category}
-                                className={`menu_button ${selectedCategory === button.data_category ? 'selected' : ''}`}
+                                className={`${style.menu_button} ${selectedCategory === button.data_category ? 'selected' : ''}`}
                                 onClick={itembtnclick}
                             />
                         ))}
                     </div>
                 </div>
-                <div className='menubox_horizon'></div>
-                <ul className='bottom_list_big_container'>
+                <div className={style.menubox_horizon}></div>
+                <ul className={style.bottom_list_big_container}>
                     {items.map((Item, index) => {
                         const itemCategory = buttondata[index % buttondata.length].data_category;
                         const isVisible = selectedCategory === null || selectedCategory === itemCategory;
-                        const itemClasses = `bottom_list_small_container ${isVisible ? '' : 'item__invisible'}`;
+                        const itemClasses = `${style.bottom_list_small_container} ${isVisible ? '' : 'item__invisible'}`;
 
                         return (
                             <div
