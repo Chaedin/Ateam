bfd92e7f69c8733066254fce0fa89dfc1940bc22
Chaerins <cherin8887@gmail.com>
2023-06-12 12:27:25 +0900

    joinus css 모듈화 완

--- src/routes/joinus.js
+++ src/routes/joinus.js
@@ -5,17 +5,17 @@ import style from '../css/joinus.module.css';
 
 const Joinus = () => {
     return (
-        <section className="main">
-            <h1 className="join_us">Join Us</h1>
+        <section className={style.main}>
+            <h1 className={style.join_us}>Join Us</h1>
 
             <form action="">
-                <div id="injoy">
+                <div id={style.injoy}>
                     <table>
                         <tbody>
                             <tr>
                                 <td>ID</td>
-                                <td className="joinusInputTd">
-                                    <div className="userId">
+                                <td className={style.joinusInputTd}>
+                                    <div className={style.userId}>
                                         <input type="text" className="id" name="id" required />
                                         <button>중복확인</button>
                                     </div>
@@ -24,8 +24,8 @@ const Joinus = () => {
 
                             <tr>
                                 <td>PASSWORD</td>
-                                <td className="joinusInputTd">
-                                    <div className="userPw">
+                                <td className={style.joinusInputTd}>
+                                    <div className={style.userPw}>
                                         <input type="password" className="pw" name="pw" required />
                                     </div>
                                 </td>
@@ -33,8 +33,8 @@ const Joinus = () => {
 
                             <tr>
                                 <td>VERIFY PASSWORD</td>
-                                <td className="joinusInputTd">
-                                    <div className="userPw">
+                                <td className={style.joinusInputTd}>
+                                    <div className={style.userPw}>
                                         <input type="password" className="pw" name="verify_pw" required />
                                     </div>
                                 </td>
@@ -42,8 +42,8 @@ const Joinus = () => {
 
                             <tr>
                                 <td>NAME</td>
-                                <td className="joinusInputTd">
-                                    <div className="userName">
+                                <td className={style.joinusInputTd}>
+                                    <div className={style.userName}>
                                         <input type="text" className="name" name="name" required />
                                     </div>
                                 </td>
@@ -51,8 +51,8 @@ const Joinus = () => {
 
                             <tr>
                                 <td>DATE OF BIRTH</td>
-                                <td className="joinusInputTd">
-                                    <div className="birth">
+                                <td className={style.joinusInputTd}>
+                                    <div className={style.birth}>
                                         <select name="year">
                                             {/* option[value="$@-1944"]{$@-1944}*80 */}
                                             {Array.from({ length: 80 }, (_, i) => {
@@ -90,8 +90,8 @@ const Joinus = () => {
 
                             <tr>
                                 <td>E-MAIL</td>
-                                <td className="joinusInputTd">
-                                    <div className="email">
+                                <td className={style.joinusInputTd}>
+                                    <div className={style.email}>
                                         <input type="text" name="emailId" required /> @
                                         <select name="email_domain">
                                             <option value="naver.com">naver.com</option>
@@ -105,8 +105,8 @@ const Joinus = () => {
 
                             <tr>
                                 <td>PHONE NUMBER</td>
-                                <td className="joinusInputTd">
-                                    <div className="userPhone">
+                                <td className={style.joinusInputTd}>
+                                    <div className={style.userPhone}>
                                         <input type="text" minLength="3" maxLength="3" size="2" required /> &#8722;
                                         <input type="text" minLength="4" maxLength="4" size="2" required /> &#8722;
                                         <input type="text" minLength="4" maxLength="4" size="2" required />
@@ -116,7 +116,7 @@ const Joinus = () => {
                         </tbody>
                     </table>
 
-                    <p className="terms">
+                    <p className={style.terms}>
                         <input type="checkbox" required />
                         <a href="../terms/terms.html" target="_blank" rel="noopener noreferrer">
                             개인정보 처리방침
@@ -124,9 +124,9 @@ const Joinus = () => {
                         에 따라 개인 정보를 수집, 사용, 타사에 대한 제공 및 처리하는 데 동의합니다.
                     </p>
 
-                    <hr className="under_terms" />
+                    <hr className={style.under_terms} />
 
-                    <button className="injoy_btn">
+                    <button className={style.injoy_btn}>
                         <Link to='/main'>회원가입</Link>
                     </button>
                 </div>
