0bd0df327c7ce05bc24a55e19a81ae6c57935b75
Chaerins <cherin8887@gmail.com>
2023-07-04 12:07:26 +0900

    mypage js, css

--- src/front/src/components/mypage/mypage.js
+++ src/front/src/components/mypage/mypage.js
@@ -1,76 +1,74 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import Mypageitem from '../mypage/mypageitem';
 import Topimg from '../topimg/topimg';
 import style from '../mypage/mypage.module.css';
 import axios from "axios";
-import {Link} from "react-router-dom";
-import Board from "../board/board";
-
+import { Link } from "react-router-dom";
 const Mypage = () => {
     const [myInfo, setMyInfo] = useState();
     const member_id = sessionStorage.getItem('loginID');
-
     // 로그인한 유저 정보
-    useEffect(()=>{
+    useEffect(() => {
         axios
             .get(`http://localhost:8080/member/myinfo?member_id=${member_id}`)
             .then(response => {
                 setMyInfo(response.data);
                 console.log(response.data)
             })
-            .catch((error)=>{
+            .catch((error) => {
                 console.error('내정보 불러오기에 실패했습니다.')
             })
-    },[])
-
+    }, [])
 
     return (
         <>
             <Topimg />
-
             <section>
                 <div className={style.myPage_container}>
                     <h1 className={style.myPageText}>My Page</h1>
-                    <div className={style.profile}>
-                        <div className={style.profile_img}></div>
-                        <ul className={style.member_info}>
-                            {myInfo ? (<li>{myInfo.member_name} 님</li>)
-                             : (<li>로그인 후 이용해주세요</li>)
-                            }
-                            {myInfo ? (<li className={style.layoutCoupon}>
-                                보유 포인트 : {myInfo.member_point}
-                            </li>) : (<li>
-                                보유 포인트 : Loading
-                            </li>)}
-
-                            <li>
-                                <Link to='/memberupdate'>
-                                    <span>회원 정보 수정</span>
-                                </Link>
-                            </li>
-                        </ul>
-                    </div>
-                    <Board member_id={member_id}></Board>
-                    <hr />
-                    <div className={style.order_check_all}>
-                        <div className={style.order}>
-                            <h2>주문내역</h2>
-                            <table>
-
-                                <tr className={style.order_info}>
-                                    <th></th>
-                                    <th>상품 이름</th>
-                                    <th>상품 가격</th>
-                                    <th>배송 정보</th>
-                                </tr>
-                                <Mypageitem></Mypageitem>
-                            </table>
+                    <div className={style.profileArea}>
+                        <div className={style.profile}>
+                            <ul className={style.member_info}>
+                                {myInfo ? (<li>{myInfo.member_name} 님 <p>({myInfo.member_role})</p></li>)
+                                    : (<li>Loading</li>)
+                                }
+                                {myInfo ? (<li><p>전화번호 : </p><p>{myInfo.member_phone}</p></li>)
+                                    : (<li>Loading</li>)
+                                }
+                                {myInfo ? (<li><p>주소 : </p><p>{myInfo.member_basic_addr}, {myInfo.member_detail_addr}</p></li>)
+                                    : (<li>Loading</li>)
+                                }
+                                {myInfo ? (<li><p>Email : </p><p>{myInfo.member_email}</p></li>)
+                                    : (<li>Loading</li>)
+                                }
+                                {myInfo ? (<li className={style.point}><p>보유 포인트 : </p><p>{myInfo.member_point} p</p></li>)
+                                    : (<li>보유 포인트 : Loading</li>)
+                                }
+                                <li>
+                                    <Link to='/memberupdate'>
+                                        <span className={style.memberupdate}>회원 정보 수정</span>
+                                    </Link>
+                                </li>
+                            </ul>
                         </div>
+                    </div>
+                    <hr />
+                    <div className={style.order_check_all}></div>
+                    <div className={style.order}>
+                        <h2>주문내역</h2>
+                        <table>
+                            <tr className={style.order_info}>
+                                <th></th>
+                                <th>상품 이름</th>
+                                <th>상품 가격</th>
+                                <th>배송 정보</th>
+                            </tr>
+                            <Mypageitem></Mypageitem>
+                        </table>
                     </div>
                 </div>
             </section>
         </>
     );
 };
-
 export default Mypage;
